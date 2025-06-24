22e23ab1cc6c9a11626789f1afce544516a04053
sahil0966 <93608226+sahil0966@users.noreply.github.com>
2025-06-25 03:33:43 +0530

    j

--- luxerazo/settings.py
+++ luxerazo/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # --------------------------------------------
 SECRET_KEY = 'django-insecure-bll9(2xl5-+nrk()f$_c!z+fqy2sk(503k4^yt0pj^&&*6_*e6'
 DEBUG = True
-ALLOWED_HOSTS = ['luxerazo.com', 'www.luxerazo.com', '3.85.26.72']
+ALLOWED_HOSTS = [ '13.220.153.37', 'luxerazo.com', 'www.luxerazo.com']
 
 # --------------------------------------------
 # INSTALLED APPS
