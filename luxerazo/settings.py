4d33811725ab5f7695da12ed95e3e9f3a60385ed
sahil0966 <93608226+sahil0966@users.noreply.github.com>
2025-06-25 06:01:41 +0530

    sa

--- luxerazo/settings.py
+++ luxerazo/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # --------------------------------------------
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
